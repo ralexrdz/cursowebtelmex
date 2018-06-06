5a3aae176e4cba117b7d92e7379bf1b43e4bfd54
Raul Rodriguez <ralex.rdz@gmail.com>
2018-06-06 11:44:06 -0500

    ejercicio cuenta palabras

---
+++ js/cuenta-palabras.js
@@ -0,0 +1,8 @@
+let cadena = "hola amigos, el otro día me encontré con mis amigos, que conocían a sus amigos"
+
+function cuentaPalabras (palabra, frase) {
+    return //cuantas palabras hay en la cadena
+}
+
+let conteo = cuentaPalabras('amigos', cadena)
+console.log('conteo', conteo)
--- js/string-methods.js
+++ js/string-methods.js
@@ -1,3 +1,21 @@
-var str = "Hola amigos, les quería decir 'hola'";
+var str = "eeey hola amigos, les quería decir, 'hola'"
 console.log(str)
-var pos = str.indexOf("locate");
+var pos = str.indexOf("hola")
+var posUltima = str.lastIndexOf('hola')
+console.log('pos', pos)
+console.log('posUltima', posUltima)
+
+var substringueado = str.substring(5,20) // pos incio, pos fin
+var substrgueado = str.substr(10,5) // pos inicio, cuantos caracteres
+console.log('substringueado', substringueado)
+console.log('substrgueado', substrgueado)
+
+var sliced = str.slice(-5) // mismo que substring pero desde atras para adelante
+console.log('sliced', sliced)
+
+var splited = str.split('hola')
+console.log('splited', splited)
+
+var replaced = str.replace(/hola/g, 'adios')
+console.log('replaced', replaced)
+

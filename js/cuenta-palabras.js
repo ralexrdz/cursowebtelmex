73508b3b11fd58af9ed2058d030f43b2c38a24af
Raul Rodriguez <ralex.rdz@gmail.com>
2018-06-06 14:18:08 -0500

    cuentaPalabrasEnFrase

--- js/cuenta-palabras.js
+++ js/cuenta-palabras.js
@@ -1,8 +1,15 @@
 let cadena = "hola amigos, el otro día me encontré con mis amigos, que conocían a sus amigos"
 
 function cuentaPalabras (palabra, frase) {
-    return //cuantas palabras hay en la cadena
+    let pos = 0
+    let cont = 0
+    while (pos > -1) {
+        pos = frase.indexOf(palabra, pos)
+        if (pos >= 0) {
+            cont++
+            pos++
+        }
+    }
+    console.log(cont)
+    return cont //cuantas palabras hay en la cadena
 }
-
-let conteo = cuentaPalabras('amigos', cadena)
-console.log('conteo', conteo)

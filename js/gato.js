de8aa399f583cbb1a8d38b3625dcdaf28df7f24d
Raul Rodriguez <ralex.rdz@gmail.com>
2018-06-11 13:52:00 -0500

    gato

---
+++ js/gato.js
@@ -0,0 +1,12 @@
+function clicked (element) {
+    console.log(element)
+}
+
+
+// console.log("cuandoElDomYaCargó", Date.now())
+// var casillas = document.getElementsByClassName("casilla")
+// console.log(casillas)
+// for (let i = 0; i < casillas.length; i++) {
+//     casillas[i].onclick = clicked
+// }
+

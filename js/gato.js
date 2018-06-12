2d85d64ddc186cd49d25ea5009ddc3f0e2a2f72d
Raul Rodriguez <ralex.rdz@gmail.com>
2018-06-12 10:26:22 -0500

    gato, json y events

--- js/gato.js
+++ js/gato.js
@@ -1,15 +1,30 @@
 let turno = 'X'
 
 function clicked (element) {
+    if (element.innerText !== '') return
     element.innerText = turno
     if (turno === 'X') {
         turno = 'O'
     } else {
         turno = 'X'
     }
+    let casillas = document.getElementsByClassName("casilla")
+    let tiros = []
+    for (let i = 0; i < casillas.length; i++) {
+        tiros.push(casillas[i].innerText)
+    }
+    let meDetengo = alguienGanoOEmpate(tiros)
+    console.log(tiros)
     console.log("Es turno de:", turno)
 }
 
+function alguienGanoOEmpate(tiros) {
+    // dime si ganó x, si ganó, si empataron o si sigue el juego
+    if (tiros[0]!== '' && tiros[0] == tiros[1] && tiros[0]==tiros[2]) {
+        if (tiros[0] === 'X') alert("gano X")
+        else alert("gano O")
+    }
+}
 
 // console.log("cuandoElDomYaCargó", Date.now())
 // var casillas = document.getElementsByClassName("casilla")

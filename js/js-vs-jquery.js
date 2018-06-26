c19696d342b81edfa1f76e32426f1a89c42a2c42
Raul Rodriguez <ralex.rdz@gmail.com>
2018-06-26 13:57:51 -0500

    icons y svg

--- js/js-vs-jquery.js
+++ js/js-vs-jquery.js
@@ -1,8 +1,9 @@
-$(function () {
-    console.log("ready")
+// On ready con JS
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JS ready")
+    
     // quiero imprimir el html dentro 
     //de cualquier elemento con clase: "mi-calse"
-
     // CON JS
     console.log('con js')
     let misClasesJS = document.getElementsByClassName("mi-clase") // HTML Collection
@@ -10,9 +11,22 @@ $(function () {
         console.log(misClasesJS[i].innerHTML)        
     }
 
+    // Agregar un hijo
+    elem.appendChild(hijo)
+})
+
+$(function () {
+    console.log("JQ ready")
+    // quiero imprimir el html dentro 
+    //de cualquier elemento con clase: "mi-calse"
     // CON JQUERY
     console.log('con jquery')
 
     $('.mi-clase').each(function (i,e) { console.log(e.innerHTML) })
     //$('.mi-clase').each((i,e) => console.log(e.innerHTML))
+
+    elem.append(hijo)
+
+
 })
+

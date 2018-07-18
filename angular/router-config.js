7789c9666625888193ebcd239a82a2c566fb22ce
Raul Rodriguez <ralex.rdz@gmail.com>
2018-07-18 13:41:32 -0500

    router example

---
+++ angular/router-config.js
@@ -0,0 +1,15 @@
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("/red", {
+        templateUrl : "red.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
